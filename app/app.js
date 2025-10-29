8cd015c5f77bfde03647c5aa77086e8fa1261287
Andrew Linzie <andrewlinzie@Andrews-MacBook-Air.local>
2025-10-29 15:13:38 -0500

    GitOps redeploy demo

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, World from Andrew!');
+  res.send('Hello, World from Drew!');
 });
 
 app.listen(8080, () => {
